66cbd1d925d2432ff402903abdd35f927abbd735
Paaless <45945412+Paaless@users.noreply.github.com>
2023-01-10 13:12:42 +0200

    clean-up and docs
    did some code clean-up(i think) and added documentation

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
-from utils import authpy
 import geopandas as gpd
+from utils import *
 import pandas as pd
-import webbrowser
-import geocoder
 import folium
-import tweepy
 import os
 
+# Authenticate to Twitter and get API object.
 api = authpy('credentials.json')
 
-#print(api)
+# Get user input for the hashtag and the number of tweets
 hashtag_to_search = '#'
 number_of_tweets = 0
 
+# Handle the case when the user enters an empty hashtag
 while (hashtag_to_search == '#'):
     hashtag_to_search +=input('Enter the hashtag you want to search:')
 
@@ -24,6 +23,7 @@ while (hashtag_to_search == '#'):
     
 print("")
 
+# Handle the case when the user enters a number that is not an integer
 while(number_of_tweets == 0):
     try:
         number_of_tweets = int(input('Enter the number of tweets you want to search:'))
@@ -33,81 +33,23 @@ while(number_of_tweets == 0):
     if (number_of_tweets == 0):
         print('You have not entered any number. Please try again!')
 
+# Search for the tweets
+tweets = gather_tweets(api, hashtag_to_search, number_of_tweets)
 
-version = 0
-while(version==0):
-    try:
-        print("1.Low resolution map")
-        print("2.High resolution map")
-        version = int(input('Enter the version of the map you want to use:'))
-        print("")
-    except ValueError:
-        print("Oops!  That was not a valid number.  Try again...")
-        
-    if (version != 1 and version != 2 and version != 0):
-        print('Please try again.')
-        version = 0
-        
-tweets = tweepy.Cursor(api.search_tweets, q=hashtag_to_search, geocode=f'{44.426765},{26.102537},1000km').items(number_of_tweets)
-location_from_tweeter = []
-location_by_user_location = []
 '''
 If the tweet has coordinates, use them, else use the coordinates of the place where the tweet was posted
 Use geocoder to get the coordinates of the location of the user and append them to the list tweets_location
 '''
 
-for tweet in tweets:
-    
-    if tweet.coordinates == None:
-        if tweet.place == None:
-            if(tweet.user.location == None):
-                print("No coordinates")
-                print("")
-            else:
-                try:
-                    coord=geocoder.osm(tweet.user.location)
-                    #print("geocoder.osm(tweet.user.location).latlng")
-                    print(coord)
-                    print("")
-                    xy=coord.latlng
-                    if(version == 1):
-                        location_by_user_location.append([xy[1],xy[0]])
-                    else:
-                        location_by_user_location.append([xy[0],xy[1]])
-                
-                except TypeError:
-                    print("No coordinates")
-    else:
-        #print("tweet.coordinates.coordinates")
-        if(version == 1):
-            tweet_coord=[tweet.coordinates['coordinates'][1],tweet.coordinates['coordinates'][0]]
-        else:
-            tweet_coord=[tweet.coordinates['coordinates'][0],tweet.coordinates['coordinates'][1]]
-        print(tweet_coord)
-        print("")
-        location_from_tweeter.append(tweet_coord)
+location_from_tweeter = []
+location_by_user_location = []
 
-filename='map.html'
-if (version == 1):
-#this is a simple map that goes with geopandas
-    df = pd.DataFrame(location_from_tweeter, columns=['lat', 'lon'])
-    #print(df)
-    geometry = [Point(xy) for xy in zip(df.lat, df.lon)]
-    gdf = GeoDataFrame(df, geometry=geometry)
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    gdf.plot(ax=world.plot(figsize=(10, 7)), marker='x', color='red', markersize=10);
-    #show the map
-    plt.title('Tweets location')
-    plt.show()
-else:
-    '''
-    Using folium to create a map with the coordinates of the tweets and then save it in a html file
-    Using webbrowser to open the html file
-    '''
-    new_map = folium.Map()
-    for i in location_from_tweeter:
-        new_map.add_child(folium.Marker(location=i,icon=folium.Icon(color='green')))
-    for j in location_by_user_location:
-        new_map.add_child(folium.Marker(location=j,icon=folium.Icon(color='red')))
-    new_map.save(filename)
-    webbrowser.open('file://' + os.path.realpath(filename))
+# Either get the coordinates from the tweet or from the place where the tweet was posted
+location_from_tweeter, location_by_user_location = return_separate_tweets(tweets)
+
+
+'''
+Using folium to create a map with the coordinates of the tweets and then save it in a html file
+Using webbrowser to open the html file
+'''
+create_map(location_from_tweeter, location_by_user_location)
